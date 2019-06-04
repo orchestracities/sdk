663a1fe22b68d04b6ceef7b874def0a2e3ea08d5
Federico M. Facca <federico.facca@gmail.com>
2019-06-04 02:31:44 +0200

    add tests for nodejs

---
+++ test-javascript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+FILES=./specs/*
+
+function yaml() {
+    hashdot=$(gem list hash_dot);
+    if ! [ "$hashdot" != "" ]; then sudo gem install "hash_dot" ; fi
+    if [ -f $1 ];then
+        cmd=" Hash.use_dot_syntax = true; hash = YAML.load(File.read('$1'));";
+        if [ "$2" != "" ] ;then 
+            cmd="$cmd puts hash.$2;"
+        else
+            cmd="$cmd puts hash;"
+        fi
+        ruby  -r yaml -r hash_dot <<< $cmd;
+    fi
+}
+
+for f in $FILES
+do
+  echo "Processing $f file..."
+  t=$(yaml $f info.title)
+  v=$(yaml $f info.version)
+  echo "Building $t API version $v for JavaScript"
+  npm install -C ./javascript/$t
+  npm link -C ./javascript/$t
+  npm run test -C ./javascript/$t
+done
